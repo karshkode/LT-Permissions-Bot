19258a7224d688d255319b2c410d20a8331e253e
Cameron Regan <cameronthomasregan@gmail.com>
2017-06-17 14:46:31 +1000

    typo

--- permsbot.rb
+++ permsbot.rb
@@ -11,7 +11,7 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 def refreshtoken(refreshtoken) # Refreshes the token, needs to be called every hour, given the refreshtoken OR every time the perms bot command is called
   uri = URI.parse("https://www.reddit.com/api/v1/access_token")
   request = Net::HTTP::Post.new(uri)
-  request.basic_auth($config["client_id"], $config["clienet_secret"]) # client_id, client_secret
+  request.basic_auth($config["client_id"], $config["client_secret"]) # client_id, client_secret
   request.set_form_data(
     "grant_type" => "refresh_token",
     "refresh_token" => refreshtoken,
@@ -62,13 +62,33 @@ def invite_user(accesstoken, redditUser, slug, permissions) # Invites a user to 
   puts response
 end
 
+def lead_user(accesstoken, redditUser, slug) # Gives a user all permissions
+  uri = URI.parse("https://www.oauth.reddit.com/api/live/#{slug}/invite_contributor/")
+
+  request = Net::HTTP::Post.new(uri.path)
+
+  request.add_field("Authorization", "bearer #{accesstoken}") # Uses our access token for OAuth2
+  request.add_field("Content-Type", "application/json")
+
+  request.form_data = {
+    "api_type" => "json",
+    "name" => redditUser,
+    "permissions" => "+update,+edit,+manage,+close,+settings,+invite",
+    "type" => "liveupdate_contributor_invite"
+  }
+
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+    http.request(request)
+  end
+  puts response
+end
+
 SlackRubyBot::Client.logger.level = Logger::WARN
-
-
-
+SLACK_API_TOKEN = "xoxb-196940761536-tjTuUeTiXZioYTnwuADLEFsc"
 class Perms < SlackRubyBot::Bot
   refreshtoken = $config["refresh_token"]
-  command 'add' do |client, data, msg|
+  command 'add' do |client, data, msg| # livebot add <slug> <user> <perms>
+    puts "Got an add command."
     msg = msg.to_s.match(/.* (.*?) (.*) (.*)/)
     slug = msg[1]
     redditUser = msg[2]
@@ -78,15 +98,16 @@ class Perms < SlackRubyBot::Bot
     accesstoken = refreshtoken(refreshtoken)
     invite_user(accesstoken, redditUser, slug, permissions)
   end
-  command 'lead' do |client, data, msg|
+  command 'lead' do |client, data, msg| # livebot lead <slug> <user>
+    puts "Got a lead command."
     msg = msg.to_s.match(/.* (.*?) (.*)/)
     slug = msg[1]
     redditUser = msg[2]
     puts slug
     puts redditUser
     accesstoken = refreshtoken(refreshtoken)
-    invite_user(accesstoken, redditUser, slug, permissions)
-    end
+    lead_user(accesstoken, redditUser, slug)
+  end
 end
 
 Perms.run
