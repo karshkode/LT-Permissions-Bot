7aa0e409d94695cdab046cff8f5a3096213ff12d
Cameron Regan <cameronthomasregan@gmail.com>
2017-06-17 18:50:32 +1000

    Organised code Added new lead_user method, etc.

--- permsbot.rb
+++ permsbot.rb
@@ -8,7 +8,11 @@ require 'yaml'
 $config = YAML.load_file('config.yaml') # Load for all secret keys and tokens
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-def refreshtoken(refreshtoken) # Refreshes the token, needs to be called every hour, given the refreshtoken OR every time the perms bot command is called
+
+# Our methods
+
+def refreshtoken(refreshtoken)
+  # Refreshes the token, needs to be called every hour, given the refreshtoken OR every time the perms bot command is called
   uri = URI.parse("https://www.reddit.com/api/v1/access_token")
   request = Net::HTTP::Post.new(uri)
   request.basic_auth($config["client_id"], $config["client_secret"]) # client_id, client_secret
@@ -34,14 +38,15 @@ def refreshtoken(refreshtoken) # Refreshes the token, needs to be called every h
       end
     end
   end
-  puts response
+  puts "#{response} in #{__method__}"
   json = JSON.parse(response.body)
   access_token = json["access_token"]
   return access_token
 end
 
 
-def invite_user(accesstoken, redditUser, slug, permissions) # Invites a user to a specified live thread, and sets specific permissions
+def invite_user(accesstoken, redditUser, slug, permissions)
+  # Invites a user to a specified live thread, and sets specific permissions
   uri = URI.parse("https://www.oauth.reddit.com/api/live/#{slug}/invite_contributor/")
 
   request = Net::HTTP::Post.new(uri.path)
@@ -59,10 +64,11 @@ def invite_user(accesstoken, redditUser, slug, permissions) # Invites a user to 
   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
     http.request(request)
   end
-  puts response
+  puts "#{response.body} in #{__method__}"
 end
 
-def lead_user(accesstoken, redditUser, slug) # Gives a user all permissions
+def lead_user(accesstoken, redditUser, slug)
+  # Check if user is already a contributor, if so, update perms, if not, invite with full perms
 
   uri = URI.parse("https://www.oauth.reddit.com/api/live/#{slug}/contributors/")
 
@@ -74,32 +80,79 @@ def lead_user(accesstoken, redditUser, slug) # Gives a user all permissions
   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
     http.request(request)
   end
-  puts response
+
+  responseparsed = JSON.parse(response.body)
+  contributors = []
+  responseparsed.each do |row|
+    row["data"]["children"].each do |child|
+      contributors << child['name']
+    end
+  end
+
+  permissions = "+update,+edit,+manage,+close,+settings,+invite" # Full perms
+
+  # This gives us a list of contributors so we can check if the redditUser is there
+  if contributors.include? redditUser # If the user is already a contributor
+    puts "#{redditUser} is already a contributor, updating permissions"
+    update_perms(accesstoken, redditUser, slug, permissions) # Then make that user the lead (give all perms)
+  else
+    puts "#{redditUser} is not a contributor, inviting with full perms"
+    invite_user(accesstoken, redditUser, slug, permissions)
+  end
 end
+
+def update_perms(accesstoken, redditUser, slug, permissions)
+  # Updates the permissions for a contributor
+  uri = URI.parse("https://www.oauth.reddit.com/api/live/#{slug}/set_contributor_permissions")
+
+  request = Net::HTTP::Post.new(uri.path)
+
+  request.add_field("Authorization", "bearer #{accesstoken}") # Uses our access token for OAuth2
+  request.add_field("Content-Type", "application/json")
+
+  request.form_data = {
+    "api_type" => "json",
+    "name" => redditUser,
+    "permissions" => permissions,
+    "type" => "liveupdate_contributor_invite"
+  }
+
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+    http.request(request)
+  end
+  puts "#{response} in #{__method__}"
+end
+
+# Slack bot part
 
 SlackRubyBot::Client.logger.level = Logger::WARN
 
 class Perms < SlackRubyBot::Bot
+
   refreshtoken = $config["refresh_token"]
+
   command 'add' do |client, data, msg| # livebot add <slug> <user> <perms>
     puts "Got an add command."
     msg = msg.to_s.match(/.* (.*?) (.*) (.*)/)
     slug = msg[1]
     redditUser = msg[2]
     permissions = msg[3]
-    puts slug
-    puts redditUser
+    puts "Thread: #{slug}"
+    puts "User: #{redditUser}"
     accesstoken = refreshtoken(refreshtoken)
+    sleep 6 # implement as an error exception for 429 so we only sleep when it fails
     invite_user(accesstoken, redditUser, slug, permissions)
   end
+
   command 'lead' do |client, data, msg| # livebot lead <slug> <user>
     puts "Got a lead command."
     msg = msg.to_s.match(/.* (.*?) (.*)/)
     slug = msg[1]
     redditUser = msg[2]
-    puts slug
-    puts redditUser
+    puts "Thread: #{slug}"
+    puts "User: #{redditUser}"
     accesstoken = refreshtoken(refreshtoken)
+    sleep 6 # implement as an error exception for 429 so we only sleep when it fails
     lead_user(accesstoken, redditUser, slug)
   end
 end
