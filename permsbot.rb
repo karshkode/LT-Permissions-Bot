43c3d22a8862ca312f5603405e7f4b4507a50a3e
Cameron Regan <cameronthomasregan@gmail.com>
2017-06-18 01:18:31 +1000

    Integrated user_check checks for Perms and admins

--- permsbot.rb
+++ permsbot.rb
@@ -17,28 +17,38 @@ class Perms < SlackRubyBot::Bot
   command 'add' do |client, data, msg| # livebot add <slug> <user> <perms>
     puts "\nGot an add command."
     userid = data["user"] # User ID of command sender
-    msg = msg.to_s.match(/.* (.*?) (.*) (.*)/)
-    slug = msg[1]
-    redditUser = msg[2]
-    permissions = msg[3]
-    puts "Thread: #{slug}"
-    puts "User: #{redditUser}"
-    accesstoken = Permissions.refreshtoken(refreshtoken)
-    sleep 3 # implement as an error exception for 429 so we only sleep when it fails
-    Permissions.invite_user(accesstoken, redditUser, slug, permissions)
+    if Permissions.user_check(userid) == true # If the list of admins (whitelist) includes the user who sent the command
+      msg = msg.to_s.match(/.* (.*?) (.*) (.*)/)
+      slug = msg[1]
+      redditUser = msg[2]
+      permissions = msg[3]
+      puts "Thread: #{slug}"
+      puts "User: #{redditUser}"
+      accesstoken = Permissions.refreshtoken(refreshtoken)
+      sleep 3 # implement as an error exception for 429 so we only sleep when it fails
+      Permissions.invite_user(accesstoken, redditUser, slug, permissions)
+    else
+      client.say(text: "You don't have the permissions to do that!", channel: data.channel)
+      puts "User didn't have permissions."
+    end
   end
 
   command 'lead' do |client, data, msg| # livebot lead <slug> <user>
     puts "\nGot a lead command."
     userid = data["user"] # User ID of command sender
-    msg = msg.to_s.match(/.* (.*?) (.*)/)
-    slug = msg[1]
-    redditUser = msg[2]
-    puts "Thread: #{slug}"
-    puts "User: #{redditUser}"
-    accesstoken = Permissions.refreshtoken(refreshtoken)
-    sleep 3
-    Permissions.lead_user(accesstoken, redditUser, slug)
+    if Permissions.user_check(userid) == true # If the list of admins (whitelist) includes the user who sent the command
+      msg = msg.to_s.match(/.* (.*?) (.*)/)
+      slug = msg[1]
+      redditUser = msg[2]
+      puts "Thread: #{slug}"
+      puts "User: #{redditUser}"
+      accesstoken = Permissions.refreshtoken(refreshtoken)
+      sleep 3
+      Permissions.lead_user(accesstoken, redditUser, slug)
+    else
+      client.say(text: "You don't have the permissions to do that!", channel: data.channel)
+      puts "User didn't have permissions."
+    end
   end
 end
 
